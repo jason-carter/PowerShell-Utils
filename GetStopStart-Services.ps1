8180b392485548d6866bb5e5b541000c3cb7c1a3
Jason Carter <Jason.Carter@rabobankdev.com>
2016-04-08 17:04:23 +0100

    A couple of new useful utilities.

---
+++ GetStopStart-Services.ps1
@@ -0,0 +1,8 @@
+﻿#Log on to admin permissioned powershell:
+#
+#    runas /user:<domain>\<AdminAccount> "powershell"
+
+
+get-service "<ServiceName>" -ComputerName <ServerName>
+stop-service $(get-service "<ServiceName>" -ComputerName <ServerName>)
+start-service $(get-service "<ServiceName>" -ComputerName <ServerName>)
