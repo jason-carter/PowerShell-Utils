8073291ff46fe8b6d0262a44418c48d4fc2408a0
Jason Carter <jjcarter@me.com>
2017-03-21 15:31:41 +0000

    Zip folders for old versions of powershell

---
+++ Examples-Zip.ps1
@@ -0,0 +1,11 @@
+﻿add-type -a system.io.compression.filesystem
+
+$basedir2zip = "\\spiderapp\spiderdata$\Reports"
+$childdir2zip = "201701"
+
+#Get-ChildItem -Directory "$($basedir2zip)\$($childdir2zip)" #\$($childdir2zip)'
+
+""
+"Archiving folder $($basedir2zip)\$($childdir2zip)"
+[io.compression.zipfile]::CreateFromDirectory("$($basedir2zip)\$($childdir2zip)", "$($basedir2zip)\$($childdir2zip).zip")
+
