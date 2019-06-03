55a2c2c1c5aeb1f57ee0eca2c01fbc2e54a247cf
Jason Carter <jason.carter@rabobank.com>
2019-06-03 16:18:12 +0100

    Tidy up the Example file snippets

--- Examples-Grep.ps1
+++
@@ -1,3 +0,0 @@
-﻿select-string -Pattern "<searchstring>" *.sql
-
-select-string -Pattern "<ss1>" *.sql | select-string -Pattern "<ss2>"
---
+++ Examples/Examples-Grep.ps1
@@ -0,0 +1,6 @@
+﻿
+#select-string  -Pattern "DataMart" *.sql
+
+#select-string -Pattern "<ss1>" *.sql | select-string -Pattern "<ss2>"
+
+Get-ChildItem -Recurse *.sql | select-string  -Pattern "DataMart"
